ce5a7aa9ecc86af1e679987a409c1d0af538841f
Andrey Deryabin <deriabin@gmail.com>
2016-12-23 09:40:53 +0300

    Add mia for gmail

---
+++ Formula/miaforgmail.rb
@@ -0,0 +1,10 @@
+cask 'miaforgmail' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://www.sovapps.com/application/notifier-pro-for-gmail/MiaForGmail.dmg'
+  homepage 'http://www.miaforgmail.com/'
+  license :unknown
+
+  app 'Mia for Gmail.app'
+end
